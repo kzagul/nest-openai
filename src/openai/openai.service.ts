6a38df49c0630d35cbf8409f405c96fbdd6a96b3
Kirill Zagul <zagulkirill@gmail.com>
2023-06-05 01:45:03 +0500

    updated prompt

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -17,54 +17,66 @@ export class OpenAIService {
   }
 
   public async getModelAnswer(
-    question: string,
+    free_time: string,
     favorite_sports: string,
+    physical_activity: string,
+    wishes: string,
     disease: string,
-    accessibleSports: string,
+    age: string,
+    gender: string,
+    accessible_sports: string,
     temperature?: number,
   ) {
     try {
       const params: CreateCompletionRequest = {
-        // prompt: `Suggest only three types of sport for person.
-        //   Basing that he/she has next favorite sports: ${favorite_sports}.
-        //   Person has next disease: ${disease}
-        //   suggest only from these sports: ${accessibleSports}`,
         prompt: `
-        "Привет! Я могу помочь тебе выбрать индивидуальный вид спорта, который подойдет именно тебе. Ответь на несколько вопросов:
+          "Добрый день! Я могу помочь вам выбрать индивидуально вид спорта, который подойдет именно вам.
+          Ответьте на несколько вопросов:
 
-        1) Что тебе нравится делать в свободное время?
-        ${favorite_sports}
+          1) Что Вам нравится делать в свободное время?
+          ${free_time}
 
-        2) Какой уровень физической активности тебе комфортен?
-        Я предпочитаю спокойные занятия в помещении, например, йогу или пилатес.
+          2) Какие Ваши любимые виды спорта
+          ${favorite_sports}
 
-        3) Что ты бы хотел достичь благодаря спорту?
-        Я хочу улучшить свою физическую форму и настроение.
+          3) Какой уровень физической активности Вам комфортен?
+          ${physical_activity}
 
-        4) Имеются ли у тебя медицинские ограничения по здоровью?
-        ${disease}
+          4) Что Вы бы хотели достичь благодаря спорту?
+          ${wishes}
 
-        5) Какой твой возраст?
-        Мне 25 лет.
+          5) Имеются ли у Вас медицинские ограничения по здоровью?
+          ${disease}
 
-        6) Какой твой пол?
-        Я мужчина.
+          6) Какой Ваш возраст?
+          ${age}
 
-        Отвечай как можно подробнее, и я порекомендую несколько подходящих видов спорта для тебя.
-        На основе твоих ответов, я могу порекомендовать наиболее подходящими для тебя пять видов спорта из списка следующих видов спорта: ${accessibleSports}
+          7) Какой Ваш пол?
+          ${gender}
 
-        Ответ дай в следующем виде: 
-        1) Первый вид спорта,  2) Второй вид спорта,  3) Третий вид спорта,  4) Четвертый вид спорта,  5) Пятый вид спорта, 
+          Отвечайте как можно подробнее, и я порекомендую несколько подходящих видов спорта для вас.
+          На основе ваших ответов, я могу порекомендовать наиболее подходящими для вас
+          пять видов спорта из списка следующих видов спорта: ${accessible_sports}
+
+          Ответ дай в следующем виде:
+          1) Первый вид спорта,
+          2) Второй вид спорта,
+          3) Третий вид спорта,
+          4) Четвертый вид спорта,
+          5) Пятый вид спорта,
         `,
 
         model: OPEN_AI_MODEL,
         temperature:
           temperature != undefined ? temperature : DEFAULT_TEMPERATURE,
+        // 0.9
         max_tokens: 256,
+        // 2048
       };
 
       const response = await this.openAIApi.createCompletion(params);
 
+      // return response.data?.choices[0]?.text;
       return response.data;
     } catch (error) {}
   }
